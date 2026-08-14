e13455ce524bbb1fa10e53a320d9a442fb3fd377
Ashit-mulani <mulaniashit@gmail.com>
2026-08-14 15:09:03 +0530

    three_sum_is_zero

---
+++ three_sum_is_zero/script.js
@@ -0,0 +1,44 @@
+const arr = [-4, -1, -1, 0, 1, 2];
+
+/**
+ * brute force :
+ * time O(n'3)
+ * space O(1)
+ */
+function brute() {
+  for (let i = 0; i < arr.length - 2; i++) {
+    for (let j = i + 1; j < arr.length - 1; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        if (arr[i] + arr[j] + arr[k] === 0) {
+          return [arr[i], arr[j], arr[k]];
+        }
+      }
+    }
+  }
+  return null;
+}
+
+/**
+ * optimized :
+ * time O(n'2)
+ * space O(1)
+ *
+ */
+function optimized() {
+  for (let i = 0; i < arr.length; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+    while (left < right) {
+      const sum = arr[i] + arr[left] + arr[right];
+      if (sum == 0) {
+        return [arr[i], arr[left], arr[right]];
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+}
+
+console.log(optimized());
